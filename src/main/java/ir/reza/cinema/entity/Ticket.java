696514004f2312936669549080bce604b516f7c8
Reza <reza.asadipour1@gmail.com>
2026-08-25 07:05:50 +0330

    Feat: implement cinema domain entities

---
+++ src/main/java/ir/reza/cinema/entity/Ticket.java
@@ -0,0 +1,72 @@
+package ir.reza.cinema.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime purchaseDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    public Ticket() {
+    }
+
+    public Ticket(LocalDateTime purchaseDate, Customer customer, Movie movie) {
+        this.purchaseDate = purchaseDate;
+        this.customer = customer;
+        this.movie = movie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", purchaseDate=" + purchaseDate +
+                ", customer=" + customer +
+                ", movie=" + movie +
+                '}';
+    }
+}
